f3f38164d38e2ed0af22ce164138af3de174ad3e
Hlb <Hlb@DESKTOP-8Q1HLA1>
2021-08-19 23:01:05 -0500

    Agregar archivos de proyecto.

---
+++ Pulsaciones/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pulsaciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char Sexo;
+            int Tiempo;
+
+            Console.Write("Digite el sexo (F/M): ");
+            Sexo = char.Parse(Console.ReadLine());
+            Console.Write("Digite el tiempo en segundos: ");
+            Tiempo = int.Parse(Console.ReadLine());
+
+            switch (Sexo)
+            {
+                case f: 
+
+            }
+    
+        }
+    }
+}
