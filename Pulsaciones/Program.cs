f9be63bb8090e8392779fe8ae8acfbf0d20ffb51
Hlbecerra <hectorluisbcrr@hotmail.com>
2021-08-20 16:18:46 -0500

    DTZ

--- Pulsaciones/Program.cs
+++ Pulsaciones/Program.cs
@@ -10,28 +10,7 @@ namespace Pulsaciones
     {
         static void Main(string[] args)
         {
-            char Sexo;
-            int Edad, Pulsaciones;
-
-            do
-            {
-                Console.WriteLine("Digite el sexo (F/M): ");
-                Sexo = char.Parse(Console.ReadLine());
-            } while (Sexo != 'f' && Sexo != 'F' && Sexo != 'm' && Sexo != 'M');
-            Console.WriteLine("Digite su edad: ");
-            Edad = int.Parse(Console.ReadLine());
-
-            if (Sexo == 'f' || Sexo == 'F')
-            {
-                Pulsaciones = (220 - Edad) / 10;
-            }
-            else if (Sexo == 'm' || Sexo == 'M')
-            {
-                Pulsaciones = (210 - Edad) / 10;
-            }
-
-            Console.WriteLine("Las pulsaciones por cada 10s son: {0}", Pulsaciones);
-            Console.ReadKey();
+            
         }
     }
 }
