e2d2832c832f1950ce4c8a79aec5c731dc57ca86
Hlbecerra <hectorluisbcrr@hotmail.com>
2021-08-20 16:24:01 -0500

    Final

--- Pulsaciones/Program.cs
+++ Pulsaciones/Program.cs
@@ -10,7 +10,23 @@ namespace Pulsaciones
     {
         static void Main(string[] args)
         {
-            
+            char Sexo;
+            int Edad;
+            int Pulsaciones;
+            Console.WriteLine("Digite el sexo (F/M): ");
+            Sexo = char.Parse(Console.ReadLine());
+            Console.WriteLine("Digite la edad");
+            Edad = int.Parse(Console.ReadLine());
+            if (Sexo.Equals('F'))
+            {
+                Pulsaciones = (220 - Edad) / 10;
+            }
+            else
+            {
+                Pulsaciones = (210 - Edad) / 10;
+            }
+            Console.WriteLine("Las pulsaciones son: " + Pulsaciones);
+            Console.ReadKey();
         }
     }
 }
